85e7ee068d431cebebecc081024f1113c9b6ab34
rico4pepe <rico4pepe@gmail.com>
2022-10-20 16:36:41 +0100

    Creating BurgerNav on the Header Component, importing close icon and using styled components to customize mui icons and also inheriting css using styled component on the section component using CSS translate style to hide and open the burger option and passing logic via props into the css adding react state (UseState) to identify the state of the burger nav to know when it true - display and false - hide

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,10 +1,13 @@
 
-import React from 'react'
+import React, {useState} from 'react'
 import styled from 'styled-components'
 import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
 
 
 function Header() {
+  // At the initaial State the burger is not opened so it is set to false
+  const [burgerStatus, setBurgerStatus] = useState(false)
   return (
    
     <Container>
@@ -20,10 +23,21 @@ function Header() {
         <RightMenu>
                 <a href = "#">Shop</a> 
                 <a href = "#">Tesla Account</a> 
-                <MenuIconContainer>
+                <MenuIconContainer onClick={() => setBurgerStatus(true)}>
                       <MenuIcon />
                   </MenuIconContainer>
         </RightMenu>
+
+                <BurgerNav status = {burgerStatus}>
+                  <CloseContainer  onClick={() => setBurgerStatus(false)}>
+                  <Close />
+                    </CloseContainer>
+                 
+                    <li> <a href = "#">Model S</a></li>
+                    <li> <a href = "#">Model Y</a></li>
+                    <li> <a href = "#">Model 3</a></li>
+                    <li> <a href = "#">Model Z</a></li>
+                  </BurgerNav>
     </Container>
   )
 }
@@ -61,4 +75,27 @@ a{
 `
 const MenuIconContainer = styled.div`
   cursor:pointer;
+`
+const BurgerNav = styled.div`
+position: fixed;
+width: 300px;
+background-color: white;
+top: 0;
+right: 0;
+bottom: 0;
+padding: 20px;
+list-style:none;
+transform: ${props => props.status ? 'translateX(0)' : 'translateX(100%)'};
+li{
+  padding: 15px 0;
+  border-bottom: 1px solid rgba(0, 0, 8, 21);
+  font-weight: 600;
+}
+`
+const CloseContainer = styled.div`
+display: flex;
+justify-content: flex-end;
+`
+const Close = styled(CloseIcon)`
+cursor:pointer;
 `
